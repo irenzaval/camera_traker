e12f78bf894fde433948b66a99d4486fbd6f9c46
irinazavaley <elenazavaley1979@gmail.com>
2025-11-08 21:33:49 +0300

    Update run.bat

--- body-pose-mobile/backend/run.bat
+++ body-pose-mobile/backend/run.bat
@@ -1,1 +1,15 @@
 
+@echo off
+echo Starting Body Pose Detection API...
+
+:: Активируем виртуальное окружение (если есть)
+if exist venv\ (
+    call venv\Scripts\activate
+)
+
+:: Устанавливаем зависимости
+pip install -r requirements.txt
+
+:: Запускаем приложение
+python app.py
+pause
